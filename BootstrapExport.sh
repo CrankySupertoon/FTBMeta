3a525bed52383dcd74d340a2ea395865b0e8c87a
Cranky Supertoon <crankysupertoon@gmail.com>
2020-09-05 20:46:56 -0400

    FTBMeta: new project page

--- BootstrapExport.sh
+++ BootstrapExport.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+#Join Working Directory
 cd $1
+
+#Remove Old HTML Files
 rm index.tmpl.html 2>/dev/null
+rm pack.tmpl.html 2>/dev/null
+
+#Fix CSS for Exported HTML
+sed -i 's/assets\/bootstrap/..\/..\/assets\/bootstrap/g' pack.html
+sed -i 's/assets\/css/..\/..\/assets\/css/g' pack.html
+
+#Convert Exported HTML For Use In GoLang
 mv index.html index.tmpl.html 2>/dev/null
+mv pack.html pack.tmpl.html 2>/dev/null
