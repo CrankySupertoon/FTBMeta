56c53fd342f8e72f44a902f8578b714f44186df4
Cranky Supertoon <crankysupertoon@gmail.com>
2020-09-06 22:16:51 -0400

    Pack info tab now works

--- BootstrapExport.sh
+++ BootstrapExport.sh
@@ -6,11 +6,15 @@ cd $1
 #Remove Old HTML Files
 rm index.tmpl.html 2>/dev/null
 rm pack.tmpl.html 2>/dev/null
+rm pack_version.tmpl.html 2>/dev/null
 
 #Fix CSS for Exported HTML
 sed -i 's/assets\/bootstrap/..\/..\/assets\/bootstrap/g' pack.html
 sed -i 's/assets\/css/..\/..\/assets\/css/g' pack.html
+sed -i 's/assets\/bootstrap/..\/..\/..\/..\/assets\/bootstrap/g' pack_version.html
+sed -i 's/assets\/css/..\/..\/..\/..\/assets\/css/g' pack_version.html
 
 #Convert Exported HTML For Use In GoLang
 mv index.html index.tmpl.html 2>/dev/null
 mv pack.html pack.tmpl.html 2>/dev/null
+mv pack_version.html pack_version.tmpl.html 2>/dev/null
